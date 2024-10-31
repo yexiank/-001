b6f7bc6b2ace72de766ebbcdaf68cfaa0e0a0254
yexiank <110823357+yexiank@users.noreply.github.com>
2024-10-31 18:06:27 +0800

    Add files via upload

---
+++ 班级大事/js/data.js
@@ -0,0 +1,46 @@
+const websiteData = {
+    carouselItems: [
+        {
+            id: 1,
+            image: 'images/1.jpg',
+            title: '班级春游活动',
+            description: '2024年春季班级春游活动剪影'
+        },
+        {
+            id: 2,
+            image: 'images/2.jpg',
+            title: '班级文化节',
+            description: '2024年班级文化节精彩瞬间'
+        }
+    ],
+    
+    classEvents: [
+        {
+            id: 1,
+            title: '班级运动会',
+            date: '2024-03-15',
+            description: '我们班在校运动会上取得了优异的成绩，获得了团体总分第一名！同学们在各个项目中都展现出了最好的状态。',
+            image: 'images/3.jpg'
+        },
+        {
+            id: 2,
+            title: '志愿服务活动',
+            date: '2024-03-01',
+            description: '组织班级同学参加社区志愿服务，为社区老人提供帮助，得到了社区居民的一致好评。',
+            image: 'images/3.jpg'
+        }
+    ],
+    
+    socialMedia: [
+        {
+            platform: '微信',
+            text: 'wang2846725356',
+            displayName: '微信号：wang2846725356'
+        },
+        {
+            platform: 'QQ',
+            text: '3071896359',
+            displayName: 'QQ号：3071896359'
+        }
+    ]
+}; 
